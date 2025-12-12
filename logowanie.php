861ac016c7d3ad717fcbd45c9e93298fb57f199e
Alan Wnuczko <alanwnuczko@protonmail.com>
2025-12-12 14:23:55 +0100

    Create logowanie.php

---
+++ logowanie.php
@@ -0,0 +1,33 @@
+<?php
+if (!isset($_POST['s']))
+{
+
+?>
+<form action="i4g2_10.php" method="post">
+    Podaj login: <input type="text" name="log"></br></br>
+    Podaj hasło: <input type="password" name="pas"></br></br></br>
+    <input type="submit" name="s" value="wyslij">
+</form>
+<?php
+}
+else
+{
+    if (strlen($_POST['log']) > 0 && strlen($_POST['pas']) > 0)
+    {
+
+        $l=$_POST['log'];
+        $l=$_POST['pas'];
+        
+        if( $l =="u1" && $p="aaa")
+        {
+            echo"Prawidłowe dane </br>";
+            header('Location: dziekujemy4ct2.php');
+        }
+        else
+        {
+            echo"Nieprawidłowe dane </br>";
+        }
+
+    }
+}
+?>
