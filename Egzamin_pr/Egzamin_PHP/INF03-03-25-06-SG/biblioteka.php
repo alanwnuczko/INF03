0cebc13fde6a82f3149ceb13356a9bd3aa07ce76
Alan <alanwnuczko@protonmail.com>
2026-03-25 15:57:49 +0100

    inf03-03-25-06-sg

---
+++ Egzamin_pr/Egzamin_PHP/INF03-03-25-06-SG/biblioteka.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styles.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BIBLIOTEKA SZKOLNA</title>
+</head>
+<body>
+    <header>
+        <h2>STRONA BIBLIOTEKI SZKOLNEJ</h2>
+    </header>
+    <section>
+        <h3>Nasze dzisiejsze propozycje:</h3>
+        <table>
+            <tr>
+                <th>Autor</th>
+                <th>Tytuł</th>
+                <th>Katalog</th>
+            </tr>
+            <?php
+                $pol = mysqli_connect("localhost", "root", "", "biblioteka");
+                $query = "SELECT autor, tytul, kod FROM ksiazki ORDER BY RAND() LIMIT 5";
+
+                if($result = $pol -> query($query)){
+                    while($row = $result -> fetch_assoc()){
+                        echo"<tr>";
+                        echo"<td>" . htmlspecialchars($row["autor"]) . "</td>";
+                        echo"<td>" . htmlspecialchars($row["tytul"]) . "</td>";
+                        echo"<td>" . htmlspecialchars($row["kod"]) . "</td>";
+                        echo"</tr>";
+                    }
+                    $result -> free();
+                }
+                mysqli_close($pol);
+            ?>
+        </table>
+    </section>
+    <main>
+        <article>
+            <img src="ksiazka1.jpg" alt="okładka książki">
+            <p>Według różnych podań najpaskudniejsza ropucha nosi w głowie piękny, cenny klejnot.</p>
+        </article>
+        <article>
+            <img src="ksiazka2.jpg" alt="okładka książki">
+            <p>Panna Stefcia i Maryla nie są to zbyt grzeczne damy, nawet nie słuchają mamy...</p>
+        </article>
+        <article>
+            <img src="ksiazka3.jpg" alt="okładka książki">
+            <p>Ratuj mnie, przyjacielu, w ostatniej potrzebie: Kocham piękną Irenę. Rodzice i ona...</p>
+        </article>
+    </main>
+    <footer>
+        <p>Stronę wykonał: <a href="https://www.github.com/alanwnuczko">Alan Wnuczko</a></p>
+    </footer>
+    
+</body>
+</html>
---
+++ Egzamin_pr/Egzamin_PHP/INF03-03-25-06-SG/kwerendy.md
@@ -0,0 +1,16 @@
+kw1:
+```sql
+SELECT nazwisko, imie, klasa, data_wypozyczenia FROM uczniowie WHERE data_wypozyczenia > "2020-03-02";
+```
+kw2:
+```sql
+SELECT nazwisko, imie, data_wypozyczenia, autor, kod FROM uczniowie INNER JOIN ksiazki ON uczniowie.id_ksiazki = ksiazki.id;
+```
+kw3:
+```sql
+DELETE FROM ksiazki WHERE autor = '0';
+```
+kw4:
+```sql
+SELECT autor, tytul, kod FROM ksiazki ORDER BY RAND() LIMIT 5;
+```
