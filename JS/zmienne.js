994267c5cb7f2cc491b98e600b98a06a32154e6f
Alan <alanwnuczko@protonmail.com>
2026-04-01 17:24:46 +0200

    wyświetlanie danych i zmienne

---
+++ JS/zmienne.js
@@ -0,0 +1,9 @@
+let wiek = 25; // Można zmienić wartość let w dalszej części
+console.log("let:", wiek);
+
+wiek = 26;
+console.log("let po zmianie:", wiek);
+
+
+const kolor = "niebieski"; // Stała
+console.log(kolor);
