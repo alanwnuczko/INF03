d60ffecad3051545f76b71fe1cbd4994af976b46
Alan <alanwnuczko@protonmail.com>
2026-04-07 12:43:32 +0200

    Update petle.php

--- PHP_Skrypty/petle.php
+++ PHP_Skrypty/petle.php
@@ -1,6 +1,7 @@
 <?php
 // IF
 echo "<h3>Pętla if:</h3>";
+
 $liczba = 8;
 if ($liczba > 10){
     echo "Liczba jest większa niż 10.<br>";
@@ -12,6 +13,7 @@ if ($liczba > 10){
 
 // WHILE
 echo "<h3>Pętla while:</h3>";
+
 $i = 1;
 while ($i <= 5){
     echo "Krok pętli: $i<br>";
@@ -20,12 +22,14 @@ while ($i <= 5){
 
 // FOR
 echo "<h3>Pętla for:</h3>";
+
 for ($j = 1; $j <= 5; $j++){
     echo "Krok pętli: $j<br>";
 }
 
 // IF w FOR
 echo "<h3>Pętla if w for:</h3>";
+
 for ($k = 1; $k <= 10; $k++){
     if ($k % 2 == 0){
         echo "$k jest parzysta<br>";
