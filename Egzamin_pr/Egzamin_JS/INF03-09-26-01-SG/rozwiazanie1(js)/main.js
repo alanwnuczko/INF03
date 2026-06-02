a6302340f170d134935a4860b9b5330bd306f23a
Alan <alanwnuczko@protonmail.com>
2026-06-02 18:28:25 +0200

    INF.03-09-26.01-SG rozwiazanie 1, 2

---
+++ Egzamin_pr/Egzamin_JS/INF03-09-26-01-SG/rozwiazanie1(js)/main.js
@@ -0,0 +1,33 @@
+function pokazKolor(){
+    document.getElementById("sekcja1").style.display = "block";
+    document.getElementById("sekcja2").style.display = "none";
+    document.getElementById("sekcja3").style.display = "none";
+
+    document.getElementById("buttonPokazKolor").style.backgroundColor = "salmon";
+    document.getElementById("buttonPokazKsztalt").style.backgroundColor = "crimson";
+    document.getElementById("buttonPokazWzor").style.backgroundColor = "crimson";
+}
+
+function pokazKsztalt(){
+    document.getElementById("sekcja1").style.display = "none";
+    document.getElementById("sekcja2").style.display = "block";
+    document.getElementById("sekcja3").style.display = "none";
+    
+    document.getElementById("buttonPokazKolor").style.backgroundColor = "crimson";
+    document.getElementById("buttonPokazKsztalt").style.backgroundColor = "salmon";
+    document.getElementById("buttonPokazWzor").style.backgroundColor = "crimson";
+}
+
+function pokazWzor(){
+    document.getElementById("sekcja1").style.display = "none";
+    document.getElementById("sekcja2").style.display = "none";
+    document.getElementById("sekcja3").style.display = "block";
+    
+    document.getElementById("buttonPokazKolor").style.backgroundColor = "crimson";
+    document.getElementById("buttonPokazKsztalt").style.backgroundColor = "crimson";
+    document.getElementById("buttonPokazWzor").style.backgroundColor = "salmon";
+}
+
+for(let i = 1; i <= 10; i++){
+    document.getElementById("sekcjaObrazow").innerHTML += `<img src="${i}.jpg" title="${i}" class="wzory">`;
+}
