677e8232674de13bd454b9d23653bfa731bda678
Alan <alanwnuczko@protonmail.com>
2026-06-08 15:47:06 +0200

    Update INF03-03-25-01-SG

--- Egzamin_pr/Egzamin_PHP/INF03-03-25-01-SG/kalendarz.php
+++ Egzamin_pr/Egzamin_PHP/INF03-03-25-01-SG/kalendarz.php
@@ -10,127 +10,111 @@
     <header>
         <h1>Dni, miesiące, lata...</h1>
     </header>
-    <div id="napis">
-        <?php
-            $pol = mysqli_connect("localhost", "root", "", "kalendarz");
 
-            $miesiac = date("m-d");
-
-            $dni_tygodnia = array(
-                'Monday'    => 'Poniedziałek',
-                'Tuesday'   => 'Wtorek',
-                'Wednesday' => 'Środa',
-                'Thursday'  => 'Czwartek',
-                'Friday'    => 'Piątek',
-                'Saturday'  => 'Sobota',
-                'Sunday'    => 'Niedziela'
-            );
-
-            $dzien_ang = date('l');
-
-            $query = "SELECT imiona FROM imieniny WHERE data = '$miesiac';";
-            $result = $pol -> query(query: $query);
-            while($row = $result -> fetch_array()){
-                echo"<p>Dzisiaj jest ".$dni_tygodnia[$dzien_ang].", ".date("d.m.y").", imieniny: $row[0]</p>";
-            }
-        ?>
-    </div>
-    <div id="left">
+    <section id="blok_napisu">
+        <p>
+            <?php
+                // Skrypt 1
+                $dzisiaj_mm_dd = date('m-d');
+                $conn = mysqli_connect("localhost", "root", "", "kalendarz");
+                $query = mysqli_query($conn, "SELECT imiona FROM imieniny WHERE data = '$dzisiaj_mm_dd'");
+                $row = mysqli_fetch_assoc($query);
+                $dzien_tygodnia = date("D");
+                if($dzien_tygodnia == 'Mon'){
+                    $dzien_tygodnia = "Poniedziałek";
+                }
+                if($dzien_tygodnia == 'Tue'){
+                    $dzien_tygodnia = "Wtorek";
+                }
+                if($dzien_tygodnia == 'Wed'){
+                    $dzien_tygodnia = "Środa";
+                }
+                if($dzien_tygodnia == 'Thu'){
+                    $dzien_tygodnia = "Czwartek";
+                }
+                if($dzien_tygodnia == 'Fri'){
+                    $dzien_tygodnia = "Piątek";
+                }
+                if($dzien_tygodnia == 'Sat'){
+                    $dzien_tygodnia = "Sobota";
+                }
+                if($dzien_tygodnia == 'Sun'){
+                    $dzien_tygodnia = "Niedziela";
+                }
+                    $data_dzisiaj = date("d-m-y");
+                echo"Dzisiaj jest {$dzien_tygodnia}, {$data_dzisiaj}, imieniny: {$row['imiona']}";
+            ?>
+        </p>
+    </section>
+    <section id="left">
         <table>
             <tr>
-                <th>Liczba dni</th>
-                <th>Miesiąc</th>
+                <th>liczba dni</th>
+                <th>miesiąc</th>
             </tr>
             <tr>
                 <td rowspan="7">31</td>
-                <td>styczeń</td>
+                <td>styczen</td>
             </tr>
-            <tr>
-                <td>marzec</td>
-            </tr>
-            <tr>
-                <td>maj</td>
-            </tr>
-            <tr>
-                <td>lipiec</td>
-            </tr>
-            <tr>
-                <td>sierpień</td>
-            </tr>
-            <tr>
-                <td>październik</td>
-            </tr>
-            <tr>
-                <td>grudzień</td>
-            </tr>
+            <tr><td>marzec</td></tr>
+            <tr><td>maj</td></tr>
+            <tr><td>lipiec</td></tr>
+            <tr><td>sierpień</td></tr>
+            <tr><td>październik</td></tr>
+            <tr><td>grudzień</td></tr>
             <tr>
                 <td rowspan="4">30</td>
                 <td>kwiecień</td>
             </tr>
-            <tr>
-                <td>czerwiec</td>
-            </tr>
-            <tr>
-                <td>wrzesień</td>
-            </tr>
-            <tr>
-                <td>listopad</td>
-            </tr>
+            <tr><td>czerwiec</td></tr>
+            <tr><td>wrzesień</td></tr>
+            <tr><td>listopad</td></tr>
             <tr>
                 <td>28 lub 29</td>
                 <td>luty</td>
             </tr>
         </table>
-    </div>
-
-    <main>
+    </section>
+    <section id="center">
         <h2>Sprawdź kto ma urodziny</h2>
         <form action="kalendarz.php" method="post">
-            <input type="date" name="data" id="data" min="2024-01-01" max="2024-12-31" required>
-            <input type="submit" value="Wyślij">
+            <input type="date" name="inputData" id="inputData" min="2024-01-01" max="2024-12-31" required>
+            <input type="submit" name="submit" value="Wyślij">
         </form>
-
         <?php
-            if(isset($_POST["data"])){
-                $data = $_POST["data"];
-                $format = date("m-d", strtotime($_POST["data"]));
-
-                $query = "SELECT imiona FROM imieniny WHERE data = '$format'";
-                $result = $pol -> query(query: $query);
-
-                while($row = $result -> fetch_array()){
-                    $imieniny = $row[0];
+            // Skrypt 2
+            if(isset($_POST['inputData']) && isset($_POST['submit'])){
+                $inputData = $_POST['inputData'];
+                $formularz_mm_dd = date("m-d", strtotime($inputData));
+                $query = mysqli_query($conn, "SELECT imiona FROM imieniny WHERE data = '$formularz_mm_dd'");
+                while($row = mysqli_fetch_assoc($query)){
+                    echo"Dnia {$inputData} są imieniny: {$row['imiona']}";
                 }
-
-                echo"$data są imieniny: $imieniny";
             }
-            mysqli_close($pol);
-        ?>
-    </main>
 
-    <div id="right">
-        <a href="https://pl.wikipedia.org/wiki/Kalendarz_Majów" target="_blank"><img src="kalendarz.gif" alt="Kalendarz Majów"></a>
+            mysqli_close($conn);
+        ?>
+    </section>
+    <section id="right">
+        <a href="https://pl.wikipedia.org/wiki/Kalendarz_Majow" target="_blank"><img src="kalendarz.gif" alt="Kalendarz Majów"></a>
         <h2>Rodzaje kalendarzy</h2>
         <ol>
-            <li>
-                Słoneczny
-                <ul>
-                    <li>kalendarz Majów</li>
-                    <li>Juliański</li>
-                    <li>Gregoriański</li>
-                </ul>
-            </li>
-            <li>
-                Księżycowy
-                <ul>
-                    <li>Starogrecki</li>
-                    <li>Babiloński</li>
-                </ul>
-            </li>
+            <li>sloneczny</li>
+            <ul>
+                <li>kalendarz Majów</li>
+                <li>juliański</li>
+                <li>gregoriański</li>
+            </ul>
+            <li>księżycowy</li>
+            <ul>
+                <li>starogrecki</li>
+                <li>babiloński</li>
+            </ul>
         </ol>
-    </div>
+    </section>
+
     <footer>
-        <p>Stronę opracował: <a href="https://www.github.com/alanwnuczko" style="color: unset;">Alan Wnuczko</a></p>
+        <p>Stronę opracował(a): <a href="https://github.com/alanwnuczko" style="color: white;">Alan Wnuczko</a></p>
     </footer>
 </body>
 </html>
